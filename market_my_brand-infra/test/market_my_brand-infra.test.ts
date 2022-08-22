519464d3b5d8e3d53f9744c656f08f97a1a2280b
Mohammad Kanth <mohammadkanth@yahoo.ca>
2022-08-22 14:08:41 -0400

    import content

---
+++ market_my_brand-infra/test/market_my_brand-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MarketMyBrandInfra from '../lib/market_my_brand-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/market_my_brand-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MarketMyBrandInfra.MarketMyBrandInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
